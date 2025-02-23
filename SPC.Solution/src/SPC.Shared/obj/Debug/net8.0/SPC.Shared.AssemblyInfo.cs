2b7f5377d95efb3a0fe2e9ebad4e6696482db85b
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-02-23 17:36:22 +0530

    Add new binary and cache files for SPC.API and SPC.Infrastructure test projects; update dependencies for .NET 8.0

--- SPC.Solution/src/SPC.Shared/obj/Debug/net8.0/SPC.Shared.AssemblyInfo.cs
+++ SPC.Solution/src/SPC.Shared/obj/Debug/net8.0/SPC.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPC.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea91062e43f5793287f87e5e12ca00f6ed910fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0a1c1c7bee35f9057c3ea6186b3eba61a7a778")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPC.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPC.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SPC.Solution/tests/SPC.Core.Tests/obj/Debug/net8.0/SPC.Core.Tests.AssemblyInfo.cs
+++ SPC.Solution/tests/SPC.Core.Tests/obj/Debug/net8.0/SPC.Core.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPC.Core.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea91062e43f5793287f87e5e12ca00f6ed910fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0a1c1c7bee35f9057c3ea6186b3eba61a7a778")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPC.Core.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPC.Core.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
