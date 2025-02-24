b580b10efeab21ce5a50fc3296b37cc886316b75
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-02-24 09:32:29 +0530

    Update binary files for SPC.API and SPC.Infrastructure projects

--- SPC.Solution/src/SPC.Core/obj/Debug/net8.0/SPC.Core.AssemblyInfo.cs
+++ SPC.Solution/src/SPC.Core/obj/Debug/net8.0/SPC.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPC.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01660e92ce06d65918e6a9063524bb800547805d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da97b6047541951c970968c48e73129520a9072e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPC.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPC.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
